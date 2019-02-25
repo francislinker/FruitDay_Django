f9cd3eb544068efd91d4df5da58ecb0b51cab837
francis <15011053797@163.com>
2019-02-25 09:04:26 +0800

    second

--- index/models.py
+++ index/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    uphone = models.CharField(max_length=11,verbose_name="手机号")
+    upwd = models.CharField(max_length=50,verbose_name='密码')
+    uemail = models.CharField(max_length=245,verbose_name='邮箱')
+    uname = models.CharField(max_length=20,verbose_name='姓名')
+    isActive = models.BooleanField(default=True)
+    def __str__(self):
+        return self.uname
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
--- index/views.py
+++ index/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def login(request):
-    return render(request,"login.html")
+    if request.method == 'GET':
+        return render(request,"login.html")
+    else:
+        uphone = request.POST['uphone']
+        upwd = request.POST['upwd']
+        users = Users.objects.filter(uphone=uphone,upwd=upwd)
+        if users:
+            return HttpResponse('欢迎：'+users[0].uname)
+        else:
+            return HttpResponse('登录失败')
 
 def cart(request):
     return render(request,"cart.html")
+
+def register(request):
+    if request.method == 'GET':
+        return render(request,"register.html")
+    else:
+        uphone = request.POST['uphone']
+        upwd = request.POST['upwd']
+        uname = request.POST['uname']
+        uemail = request.POST['uemail']
+
+        users = Users.objects.filter(uphone=uphone)
+        if users:
+            err = '电话号码已经存在'
+            return render(request,'register.html',locals())
+
+        Users.objects.create(uphone=uphone,upwd=upwd,uname=uname,uemail=uemail)
+        return redirect('/')
+
